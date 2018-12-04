4d2f6134e459cad9a377a5b5e8e1bb719effc558
daneecool <39431521+daneecool@users.noreply.github.com>
2018-12-04 16:01:56 +0900

    Update testcircuit.py

--- testcircuit.py
+++ testcircuit.py
@@ -24,7 +24,7 @@ GPIO.setup(MAGNETICDOORSWITCH3V3_PIN_SIG , GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(PUSHBUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Set up the solenoid pin.
-GPIO.setup(PULLPUSHSOLENOID_PIN_SIG , GPIO.IN , pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(PULLPUSHSOLENOID_PIN_SIG , GPIO.OUT , initial=GPIO.LOW)
 
 # Make sure all lights are off.
 GPIO.output(LEDR_PIN_VIN , False)
@@ -43,19 +43,22 @@ def cleanupLights(signal, frame):
 signal.signal(signal.SIGINT, cleanupLights) 
 
 while True:
-    input_state = GPIO.input(PULLPUSHSOLENOID_PIN_SIG)  
+    input_state = GPIO.input(PUSHBUTTON_PIN)  
     if (input_state == False):
         aldOpen = isOpen
-        isOpen = GPIO.input(MAGNETICDOORSWITCH3V3_PIN_SIG and PUSHBUTTON_PIN)
+        isOpen = GPIO.input(MAGNETICDOORSWITCH3V3_PIN_SIG)
         if (isOpen and (isOpen != aldOpen)):
+            GPIO.output(PULLPUSHSOLENOID_PIN_SIG , GPIO.HIGH)
             GPIO.output(LEDR_PIN_VIN , False)
             GPIO.output(LEDG_PIN_VIN , True)
             print ("Door is UNLOCK")
         elif (isOpen != aldOpen):
+            GPIO.output(PULLPUSHSOLENOID_PIN_SIG , GPIO.LOW)
             GPIO.output(LEDG_PIN_VIN , False)
             GPIO.output(LEDR_PIN_VIN , True)
             print ("Door is LOCK")
     elif (input_state == True):
+        GPIO.output(PULLPUSHSOLENOID_PIN_SIG , GPIO.HIGH)
         GPIO.output(LEDG_PIN_VIN , True)
         GPIO.output(LEDR_PIN_VIN , False)
         print("Push button is pressed!, door unlcoked")
